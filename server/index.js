56288dfd4b61a391ebe41e1ffd134ff277b1d2a2
ShowGa <peter1997217123@gmail.com>
2024-02-25 15:43:03 +0800

    Connect to database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1/EstateDB")
+  .then(() => {
+    console.log("Successfully connect to MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.get("/", (req, res) => {
   res.send("HelloW Bish !");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("Server listening to Port 3000");
 });
