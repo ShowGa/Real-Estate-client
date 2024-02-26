83f3d534cea0aceacbd6b6bf7cee136045b8e581
ShowGa <peter1997217123@gmail.com>
2024-02-26 17:28:54 +0800

    Create sign in API route

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,5 +16,26 @@ export const signup = async (req, res, next) => {
   } catch (e) {
     console.log("authCon signup catch error !");
     next(e);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // authenticate the email
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found !"));
+    // authenticate the password
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credential"));
+
+    // Sign website token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // I don't know what is this
+    const { password: PS, ...rest } = validUser._doc;
+    // Save in the cookie
+    return res.cookie("access_token", token, { httpOnly: true }).json(rest);
+  } catch (e) {
+    console.log(e);
   }
 };
