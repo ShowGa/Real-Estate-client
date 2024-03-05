d79502fc102ebe9df513d2471203058370dde89c
ShowGa <peter1997217123@gmail.com>
2024-03-05 22:59:47 +0800

    Create get user listings API route

--- server/controller/listingController.js
+++ server/controller/listingController.js
@@ -1,9 +1,24 @@
 import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const newListing = await Listing.create(req.body);
     return res.status(201).json(newListing);
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "Sneaky peaky peeping ?"));
+  }
+
+  try {
+    const userRef = req.params.id;
+    const listings = await Listing.find({ userRef });
+    return res.status(200).json(listings);
   } catch (e) {
     next(e);
   }
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { createListing } from "../controller/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { getUserListing } from "../controller/listingController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+
+router.get("/listings/:id", verifyToken, getUserListing);
 
 export default router;
